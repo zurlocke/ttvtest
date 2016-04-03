269af1a0c92cb566ea8d35434b9d88007441d922
zurlocke <fnpocontact@gmail.com>
2016-04-03 07:49:32 -0400

    Update ttvtester.chat.js

--- ttvtester.chat.js
+++ ttvtester.chat.js
@@ -6,7 +6,7 @@
 // @author       zurlocke
 // @include      https://www.twitch.tv/directory/following
 // @updateURL    https://github.com/zurlocke/ttvtest/raw/master/ttvtester.chat.js
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
 
 $("nav.js-nav").prepend(<li><a>"Test123"</a></li>;)
