42a28aacff109fba03773cf0475f472e1702228c
zurlocke <fnpocontact@gmail.com>
2016-04-03 07:46:54 -0400

    Update ttvtester.chat.js

--- ttvtester.chat.js
+++ ttvtester.chat.js
@@ -8,3 +8,5 @@
 // @updateURL    https://github.com/zurlocke/ttvtest/raw/master/ttvtester.chat.js
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
+
+$("nav.js-nav").prepend(<li><a>"Test123"</a></li>;)
