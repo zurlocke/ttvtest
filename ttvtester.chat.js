b1d1467ac6190a276032d079917e7b776c20bfa7
zurlocke <fnpocontact@gmail.com>
2016-04-03 06:02:18 -0400

    Update ttvtester.chat.js

--- ttvtester.chat.js
+++ ttvtester.chat.js
@@ -1,1 +1,10 @@
-
+// ==UserScript==
+// @name         ttvtest
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  ttvtesting
+// @author       zurlocke
+// @include      https://www.twitch.tv/directory/following
+// @updateURL    https://github.com/zurlocke/ttvtest/raw/master/ttvtester.chat.js
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// ==/UserScript==
